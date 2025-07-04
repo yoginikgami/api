9b578d40531eca782b43613d4d1b96d3c92cfa54
yoginikgami <yoginigami39@gmail.com>
2025-07-04 18:01:12 +0530

    post

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 
 class PostController extends Controller
 {
@@ -12,7 +17,13 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $data['posts'] = Post::all();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'All Post Data.',
+            'data' => $data,
+        ],200);
     }
 
     /**
@@ -20,7 +31,34 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validateUser = Validator::make(
+    $request->all(),
+    [
+                'title' => 'required',
+                'description'=> 'requiredl',
+                'image'=> 'required|mimes:png,jpg,jpeg,gif',
+            ]
+        );
+
+        if($validateUser->fails()){
+            return response()->json([
+                'status'=> false,
+                'message'=> 'Validation Error',
+                'errors' => $validateUser->errors()->all()
+            ],401);
+        }
+
+        $post = Post::create([
+            'title'=> $request->title,
+            'description'=> $request->description,
+            'image'=> $imageName,
+        ]);
+
+        return response()->json([
+            'status'=> true,
+            'message' => '',
+            'post' => $post,
+        ],200);
     }
 
     /**
